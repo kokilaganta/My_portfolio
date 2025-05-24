9aee20e21bb4c072af2be2b1aa39904838a06d94
ganta kokila <gantakokilaa@gmail.com>
2025-05-24 14:52:02 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,22 +11,33 @@ class Projects extends React.Component {
                 </div>
                 <Projectcomponent
                     name="WhrilMail"
-                    code=""
+                    code="https://github.com/kokilaganta/whrilmail"
                     desc="WhirlMail is a simple yet powerful Flask-based web app for sending mass emails 
                     with ease. Upload a CSV of recipients, compose your subject and message, and let WhirlMail handle the rest.
 Simple Flask app to send mass emails from a CSV list
 Upload file, customize subject and message, and send instantly
 Validates emails and shows which addresses are invalid"
                     imag="https://i.pinimg.com/736x/60/b5/b7/60b5b7635eec5c7c8e353eb09b23b9d0.jpg"
+                    link=""
                 />
                 <Projectcomponent
                     name="DiaScan"
-                    code=""
+                    code="https://github.com/kokilaganta/DiaScan"
                     desc="DiaScan is a web app developed using Python and machine learning algorithms to help 
                     users assess their risk of diabetes. By analyzing user inputs, 
                     it provides quick and accurate predictions to support early detection and proactive health management"
                     imag="https://i.pinimg.com/736x/73/f3/af/73f3af5244cc0e0d29b8ad528a62c70a.jpg"
                 />
+                <Projectcomponent
+                    name="AttriSense"
+                    code="https://predictemployeeattrition.onrender.com"
+                    desc="AttriSense is a Flask-based web app that predicts employee attrition using machine learning.
+Upload employee data and get instant insights into who might leave and why.
+Visualize key factors affecting attrition through clean, interactive charts.
+Empower HR decisions with smart, data-driven predictions for better retention."
+                    imag="https://i.pinimg.com/736x/78/bf/85/78bf8595a192b256854073f2ec9d279a.jpg"
+                />
+               
                
 
             </div>
